b5445cc6d11f7d28b60547078b56a2a32b8cd200
dibragimov <dilshod.ibragimov@gmail.com>
2023-11-30 17:16:04 +0500

    model file is too big, untracking from github, train it first and specify the location in settings file

--- ModelServing/main.py
+++ ModelServing/main.py
@@ -1,15 +1,14 @@
-# from dynaconf import settings
 import io
 from dynaconf import Dynaconf
 import tensorflow as tf
 import base64
 from PIL import Image
 import numpy as np
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+# from pydantic import BaseModel
 from contextlib import asynccontextmanager
 
 
@@ -27,15 +26,11 @@ async def load_clf(app: FastAPI):
 
 
 app = FastAPI(title="Predicting Image Class", lifespan=load_clf)
+# allow traffic from everywhere
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True,
                    allow_methods=['*'], allow_headers=['*'])
 inception_model = None
 class_labels = {0: 'birds', 1: 'cats', 2: 'dogs'}
-
-
-# Represents a particular image (or datapoint)
-class Img(BaseModel):
-    content: list = []
 
 
 @app.get("/")
@@ -45,22 +40,27 @@ def home():
 
 @app.post("/predict")
 async def predict(request: Request):
+    # read json
     data = await request.json()
+    # get the content from list (for now it contains one item)
     data_image = data['image'][0]
     data_image = str(data_image)
+    # if has metadata - remove it
     if 'base64,' in data_image:  # remove this first part containing image info
         metadata, data_image = data_image.split('base64,', 1)
         print('metadata', metadata)
     image_bytes = base64.b64decode(data_image)
     # print(image_bytes)
+    # convert to image
     image = Image.open(io.BytesIO(image_bytes))
     image = image.convert('RGB')
     image = image.resize((150,150), Image.NEAREST)
+    # preprocess with Keras
     image = tf.keras.preprocessing.image.img_to_array(image)
     data_point = np.expand_dims(image, axis=0)
-
+    # do prediction
     pred = inception_model.predict(data_point/255.0).tolist()
-    pred = pred[0]
+    pred = pred[0]  # first value as we have only 1 image
     classid = np.argmax(pred, axis=0)
     print('predictions', pred, max(pred), classid, class_labels[classid])
     return JSONResponse(jsonable_encoder({
